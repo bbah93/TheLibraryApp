23f5db2ee7bace566e80b238afe3e3b138f23a7e
Bouabacar <boubacarbah@ac.c4q.nyc>
2018-10-13 22:07:37 -0400

    added menu, Recycler, and Fragments

--- app/src/main/java/nyc/bbah/thelibraryapp/MainActivity.kt
+++ app/src/main/java/nyc/bbah/thelibraryapp/MainActivity.kt
@@ -2,11 +2,19 @@ package nyc.bbah.thelibraryapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
